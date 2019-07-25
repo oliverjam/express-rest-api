83ce848ad7526305f3e98dd333e319ba062cd2cf
oliver.phillips <oliver.phillips@ticketmaster.co.uk>
2019-07-25 15:43:25 +0100

    Move db operations into connect file

--- model/todos.js
+++ model/todos.js
@@ -1,16 +1,13 @@
-const { writeFile } = require("fs").promises;
 const uuid = require("uuid/v1");
 const db = require("../db/connect");
 
 module.exports = { get, post };
 
 function get(id) {
-  const todo = db.todos.find(todo => id === todo.id);
-  return todo;
+  return db.get("todos", todo => id === todo.id);
 }
 
 function post(newTodo) {
   const id = uuid();
-  db.todos.push({ ...newTodo, id });
-  return writeFile("db.json", JSON.stringify(db), "utf8").then(() => id);
+  return db.push("todos", { ...newTodo, id }).then(() => id);
 }
